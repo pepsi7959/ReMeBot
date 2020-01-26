2fb54eb5d9a85ad39bd80610597d7411ee256427
pepsi7959 <narongsak.mala@gmail.com>
2020-01-26 21:15:44 +0700

    extending length of fields

--- database/scripts/create-meeting-room.sql
+++ database/scripts/create-meeting-room.sql
@@ -1,14 +1,14 @@
 CREATE TABLE `MeetingRoom` (
-  `id` int(16) NOT NULL,
+  `id` int(11) NOT NULL,
   `room` varchar(64) NOT NULL,
   `meeting_begin` datetime NOT NULL,
   `meeting_end` datetime NOT NULL,
   `reserver` varchar(64) NOT NULL,
   `internal_tel` varchar(45) DEFAULT NULL,
-  `division` varchar(45) NOT NULL,
-  `department` varchar(45) DEFAULT NULL,
+  `division` varchar(128) NOT NULL,
+  `department` varchar(128) DEFAULT NULL,
   `agenda` varchar(256) DEFAULT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
   UNIQUE KEY `idMeetingRoom_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
