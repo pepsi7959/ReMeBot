bfc086f0bb9a9e3748a08ba1707a1de36b78b58b
pepsi7959 <narongsak.mala@gmail.com>
2020-03-20 14:49:36 +0700

    fixed invalid checking start time

---
+++ config/production.conf-template.js
@@ -0,0 +1,9 @@
+module.exports = {
+    MYSQL_host: "192.168.17.164",
+    MYSQL_port: 3306,
+    MYSQL_user: "seminar",
+    MYSQL_password: "Acho20mkr",
+    MYSQL_database: "remebot",
+
+    applicationLog: 'application.log'
+};
